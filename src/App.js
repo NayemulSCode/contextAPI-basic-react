6e23c30f36ad3af7575d98b3ed9c790afa9368de
Nayemul <nayemulhasan2016@gmail.com>
2021-03-07 21:21:08 +0600

    usecontext in multiple component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { createContext, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
+export const CategoryContext = createContext();
+
 function App() {
+  const [category, setCategory] = useState(0);
   return (
-    <>
+    <CategoryContext.Provider value={[category, setCategory]} >
       <Header />
       <Home />
-    </>
+    </CategoryContext.Provider>
   );
 }
 
